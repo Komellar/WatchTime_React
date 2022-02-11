7159dcf633d5d3c232fdbc54c700f55618c43090
Adrian Konopnicki <adriankonopnicki@wp.pl>
2022-02-11 17:32:29 +0100

    Updates

--- src/components/UI/Space.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const Space = styled.div`
-  height: ${(p) => p.height};
-`;
-
-export default Space;
--- src/hooks/use-random.js
+++
@@ -1,6 +0,0 @@
-const useRandom = () => {
-  const randomId = Math.floor(Math.random() * 400 + 1);
-  return randomId;
-};
-
-export default useRandom;
